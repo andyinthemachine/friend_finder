4df7edfc4e6a94ae825e7c591d9da71d715294da
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-07-07 15:29:29 -0600

    compatibilty logic in

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,10 +4,34 @@
 
 var friends = [
     {
-        "route_name": "bill",
-        "name": "Bill",
-        "photo": "",
-        "scores": ['5', '4', '3', '2', '1', '5', '4', '3', '2', '1']
+        "route_name": "bill1",
+        "name": "Bill 1",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "scores": ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+    },
+    {
+        "route_name": "bill2",
+        "name": "Bill 2",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "scores": ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2']
+    },
+    {
+        "route_name": "bill3",
+        "name": "Bill 3",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "scores": ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
+    },
+    {
+        "route_name": "bill4",
+        "name": "Bill 4",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "scores": ['4', '4', '4', '4', '4', '4', '4', '4', '4', '4']
+    },
+    {
+        "route_name": "bill5",
+        "name": "Bill 5",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "scores": ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
     }
 ];
 // reminder - find out why object with array of nums posted comes back as strings
