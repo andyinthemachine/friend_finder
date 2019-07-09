bc1a2ab40531ec87668419f8fe017afd10d39200
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-07-09 12:03:36 -0600

    express linking added

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app"));
 
 require("./app/routing/api_routes.js")(app);
 require("./app/routing/html_routes.js")(app);
